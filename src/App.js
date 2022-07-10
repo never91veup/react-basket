35557cf82ae4ac5988d65fac2862394d42c418eb
never91veup <vp.vlad00@mail.ru>
2022-07-10 11:02:18 +0300

    added without sale and form-add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.webp',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'wall-light.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'light',
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        }
+      ]
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
+        <Footer />
+      </div>
+    )
+  }
+
+  deleteOrder(id) {
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+        isInArray = true
+    })
+    if(!isInArray)
+    this.setState({orders: [...this.state.orders, item] })
+  }
+
 }
 
 export default App;
